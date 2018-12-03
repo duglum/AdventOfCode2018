b3c79ab04783468be67a420f1d01ddde93932eb6
Georg Brzyk <georg@brzyk.de>
2018-12-03 09:04:38 +0100

    Added Day03 Part 2 and tests

---
+++ Day03/Fabric.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day03
+{
+    class Fabric
+    {
+        static void Main(string[] args)
+        {
+            var input = ReadInputFile();
+            MultipleClaims(input, out var multipleClaims, out var wholeId);
+            Console.WriteLine($"Multiple claims: {multipleClaims}");
+            Console.WriteLine($"Whole ID: {wholeId}");
+        }
+
+        public static void MultipleClaims(IEnumerable<string> inputList, out int multiple, out int wholeId)
+        {
+            var array = FillFabric(inputList);
+            multiple = GetMultipleClaims(array);
+            wholeId = GetIdWithoutOverlap(array, inputList);
+        }
+
+        internal static int[,] FillFabric(IEnumerable<string> inputList)
+        {
+            var array = new int[1000, 1000];
+            foreach (var input in inputList)
+            {
+                ParseInput(input, out var id, out var left, out var top, out var width, out var height);
+                for (var i = left; i < left + width; i++)
+                {
+                    for (var j = top; j < top + height; j++)
+                    {
+                        array[i, j]++;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        internal static int GetMultipleClaims(int[,] fabric)
+        {
+            var multiple = 0;
+            for (var i = 0; i < 1000; i++)
+            {
+                for (var j = 0; j < 1000; j++)
+                {
+                    if (fabric[i, j] > 1)
+                    {
+                        multiple++;
+                    }
+                }
+            }
+            return multiple;
+        }
+
+        internal static int GetIdWithoutOverlap(int[,] fabric, IEnumerable<string> inputList)
+        {
+            var wholeId = -1;
+            foreach (var input in inputList)
+            {
+                var whole = true;
+                ParseInput(input, out var id, out var left, out var top, out var width, out var height);
+                for (var i = left; i < left + width; i++)
+                {
+                    for (var j = top; j < top + height; j++)
+                    {
+                        if (fabric[i, j] != 1)
+                        {
+                            whole = false;
+                        }
+                    }
+                }
+
+                if (whole)
+                {
+                    wholeId = id;
+                }
+            }
+
+            return wholeId;
+        }
+
+        internal static void ParseInput(string input, out int id, out int left, out int top, out int width, out int height)
+        {
+            id = int.Parse(input.Split('@')[0].Trim().Substring(1));
+            var coordinates = input.Split('@')[1].Split(':')[0].Trim().Split(',');
+            left = int.Parse(coordinates[0]);
+            top = int.Parse(coordinates[1]);
+            var rectangle = input.Split('@')[1].Split(':')[1].Trim().Split('x');
+            width = int.Parse(rectangle[0]);
+            height = int.Parse(rectangle[1]);
+        }
+
+        private static IEnumerable<string> ReadInputFile()
+        {
+            return File.ReadAllLines("Input.txt").ToList();
+        }
+    }
+}
---
+++ Day03/FabricTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Day03
+{
+    [TestFixture]
+    public class FabricTests
+    {
+        private static readonly List<string> inputList = new List<string>
+        {
+            "#1 @ 1,3: 4x4",
+            "#2 @ 3,1: 4x4",
+            "#3 @ 5,5: 2x2"
+        };
+
+        [Test]
+        public void Parse()
+        {
+            Fabric.ParseInput(inputList[0], out var id, out var left, out var top, out var width, out var height);
+            Assert.That(id == 1);
+            Assert.That(left == 1);
+            Assert.That(top == 3);
+            Assert.That(width == 4);
+            Assert.That(height == 4);
+        }
+
+        [Test]
+        public void FillTest()
+        {
+            var fabric = Fabric.FillFabric(inputList);
+            // spot check
+            Assert.That(fabric[1,4] == 1);
+            Assert.That(fabric[4,1] == 1);
+            Assert.That(fabric[4,4] == 2);
+        }
+
+        [Test]
+        public void MultipleTest()
+        {
+            var fabric = Fabric.FillFabric(inputList);
+            var multiple = Fabric.GetMultipleClaims(fabric);
+            Assert.That(fabric[4,4] >= 1);
+            Assert.That(fabric[4,5] >= 1);
+            Assert.That(fabric[5,4] >= 1);
+            Assert.That(fabric[5,5] >= 1);
+            Assert.That(multiple == 4);
+        }
+
+        [Test]
+        public void OverlapTest()
+        {
+            var fabric = Fabric.FillFabric(inputList);
+            var idNoOverlap = Fabric.GetIdWithoutOverlap(fabric, inputList);
+            Assert.That(idNoOverlap == 3);
+        }
+    }
+}
