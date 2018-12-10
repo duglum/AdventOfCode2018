7a276b735b6ac6b4a5332503d1d90c0c09e7d4be
Georg Brzyk <georg@brzyk.de>
2018-12-10 20:39:14 +0100

    Added Day 10

---
+++ Day10/Navigation.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day10
+{
+    class Navigation
+    {
+        static void Main(string[] args)
+        {
+            var input = ReadInputFile();
+            Part1(input);
+        }
+
+        public static void Part1(IEnumerable<string> input)
+        {
+            var points = input.Select(Point.Parse).ToList();
+            var step = 0;
+
+            var boundingBox = (x: 100, y: 50);
+
+            while (true)
+            {
+                foreach (var point in points)
+                {
+                    point.Step();
+                }
+
+                var maxX = points.Max(p => p.X);
+                var minX = points.Min(p => p.X);
+                var width = Math.Abs(maxX - minX);
+                var maxY = points.Max(p => p.Y);
+                var minY = points.Min(p => p.Y);
+                var height = Math.Abs(maxY - minY);
+                step++;
+
+                if (width < boundingBox.x && height < boundingBox.y)
+                {
+                    Console.WriteLine($"Step {step}");
+                    for (var i = minY; i <= maxY; i++)
+                    {
+                        for (var j = minX; j <= maxX; j++)
+                        {
+                            Console.Write(points.Any(p => p.Y == i && p.X == j) ? "#" : ".");
+                        }
+
+                        Console.WriteLine();
+                    }
+
+                    var key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Enter)
+                        break;
+                }
+                
+                if (step % 1000 == 0)
+                    Console.WriteLine($"Step {step}");
+            }
+        }
+
+        private static IEnumerable<string> ReadInputFile()
+        {
+            return File.ReadAllLines("Input.txt");
+        }
+    }
+
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        private int VX { get; set; }
+        private int VY { get; set; }
+        
+        public static Point Parse(string line)
+        {
+            var input = line.Split(',');
+            var p = new Point
+            {
+                X = int.Parse(input[0].Substring(10)),
+                Y = int.Parse(input[1].Split('>')[0]),
+                VX = int.Parse(input[1].Split('<')[1]),
+                VY = int.Parse(input[2].Split('>')[0])
+            };
+            return p;
+        }
+
+        public void Step()
+        {
+            X += VX;
+            Y += VY;
+        }
+    }
+}
---
+++ Day10/NavigationNotWorking.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day10
+{
+    class NavigationNotWorking
+    {
+        static void MainNotWorking(string[] args)
+        {
+            var input = ReadInputFile();
+            Part1(input);
+        }
+
+        public static void Part1(IEnumerable<string> input)
+        {
+            var points = input.Select(PointNotWorking.Parse).ToList();
+            var maxX = points.Max(p => p.Coordinate.x);
+            var maxY = points.Max(p => p.Coordinate.y);
+            var minX = points.Min(p => p.Coordinate.x);
+            var minY = points.Min(p => p.Coordinate.y);
+            var sizeX = Math.Abs(maxX - minX);
+            var sizeY = Math.Abs(maxY - minY);
+            var fieldSize = Math.Max(sizeX, sizeY);
+            var center = fieldSize / 2;
+            var step = 0;
+
+            while (true)
+            {
+                var field = new char[fieldSize, fieldSize];
+                foreach (var point in points)
+                {
+                    var coordinateX = center + point.Coordinate.x + (step * point.Velocity.x);
+                    var coordinateY = center + point.Coordinate.y + (step * point.Velocity.y);
+                    field[coordinateY, coordinateX] = '#';
+                }
+
+                Console.WriteLine($"Step {step}");
+                for (var i = 0; i < fieldSize; i++)
+                {
+                    for (var j = 0; j < fieldSize; j++)
+                    {
+                        Console.Write($"{field[i, j]} ");
+                    }
+
+                    Console.WriteLine();
+                }
+
+                step++;
+
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Enter)
+                    break;
+            }
+        }
+
+        private static IEnumerable<string> ReadInputFile()
+        {
+            return File.ReadAllLines("Input.txt");
+        }
+    }
+
+    public class PointNotWorking
+    {
+        public (int x, int y) Coordinate { get; set; }
+        public (int x, int y) Velocity { get; set; }
+
+        public static PointNotWorking Parse(string line)
+        {
+            var input = line.Split(',');
+            var cx = int.Parse(input[0].Substring(10));
+            var cy = int.Parse(input[1].Split('>')[0]);
+            var vx = int.Parse(input[1].Split('<')[1]);
+            var vy = int.Parse(input[2].Split('>')[0]);
+            var p = new PointNotWorking
+            {
+                Coordinate = (cx, cy),
+                Velocity = (vx, vy)
+            };
+            return p;
+        }
+    }
+}
