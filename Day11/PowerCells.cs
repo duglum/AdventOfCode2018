94c40fcf9d1b4f8d5630a293cfe3991d6f2dbb85
Georg Brzyk <georg@brzyk.de>
2018-12-11 13:04:01 +0100

    Added Day 11

---
+++ Day11/PowerCells.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Day11
+{
+    class PowerCells
+    {
+        static void Main(string[] args)
+        {
+            var (x, y, power) = Part1(6392);
+            Console.WriteLine($"Coordinate: {x}/{y} with power {power}");
+
+            var (x2, y2, power2, squareSize) = Part2(6392);
+            Console.WriteLine($"Coordinate: {x2}/{y2} with power {power2}, squareSize {squareSize}");
+        }
+
+        public static (int x, int y, int power) Part1(int input)
+        {
+            var field = CreatePowerField(input);
+            return CalculatePowerForSquare(6392, field, 3);
+        }
+
+        public static (int x, int y, int power, int squareSize) Part2(int input)
+        {
+            var field = CreatePowerField(input);
+            var largestPower = 0;
+            var largestCoordinate = (x: 0, y: 0);
+            var squareSize = 0;
+            for (var i = 1; i <= 300; i++)
+            {
+                Console.WriteLine($"Calculating for {i}");
+                var (x, y, power) = CalculatePowerForSquare(input, field, i);
+                if (power > largestPower)
+                {
+                    Console.WriteLine($"New largest power: {power}, at coordinate ({largestCoordinate.x}, {largestCoordinate.y})");
+                    largestPower = power;
+                    largestCoordinate.x = x;
+                    largestCoordinate.y = y;
+                    squareSize = i;
+                }
+            }
+
+            return (largestCoordinate.x, largestCoordinate.y, largestPower, squareSize);
+        }
+
+        public static (int x, int y, int power) CalculatePowerForSquare(int input, int[,] field, int squareSize)
+        {
+            // find largest 3x3 square
+            var largestCombinedPower = 0;
+            var largestPowerCoordinate = (x: 1, y: 1);
+            for (var i = 1; i <= 300 - squareSize; i++)
+            {
+                for (var j = 1; j <= 300 - squareSize; j++)
+                {
+                    var sum = 0;
+                    for (var k = 0; k < squareSize; k++)
+                    {
+                        for (var l = 0; l < squareSize; l++)
+                        {
+                            sum += field[k + i, l + j];
+                        }
+                    }
+
+                    if (sum > largestCombinedPower)
+                    {
+                        largestCombinedPower = sum;
+                        largestPowerCoordinate = (i, j);
+                    }
+                }
+            }
+
+            return (largestPowerCoordinate.x, largestPowerCoordinate.y, largestCombinedPower);
+        }
+
+        internal static int[,] CreatePowerField(int input)
+        {
+            // fill field with power levels
+            var field = new int[301, 301];
+            for (var i = 1; i <= 300; i++)
+            {
+                for (var j = 1; j <= 300; j++)
+                {
+                    field[i, j] = GetPowerLevel(i, j, input);
+                }
+            }
+
+            return field;
+        }
+
+        internal static int GetPowerLevel(int i, int j, int input)
+        {
+            var rackId = i + 10;
+            var powerLevelStr = ((rackId * j + input) * rackId).ToString();
+            var powerLevel = int.Parse(powerLevelStr.Substring(powerLevelStr.Length - 3, 1)) - 5;
+            return powerLevel;
+        }
+    }
+}
---
+++ Day11/PowerCellsTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Day11
+{
+    [TestFixture]
+    public class PowerCellsTests
+    {
+        [Test]
+        [TestCase(3, 5, 8, 4)]
+        [TestCase(122, 79, 57, -5)]
+        [TestCase(217, 196, 39, 0)]
+        [TestCase(101, 153, 71, 4)]
+        public void GetPowerLevelTest(int i, int j, int input, int result)
+        {
+            PowerCells.GetPowerLevel(i, j, input).Should().Be(result);
+        }
+
+        [Test]
+        [TestCase(18, 90, 269, 16, 113)]
+        [TestCase(42, 232, 251, 12, 119)]
+        public void GridTest(int input, int x, int y, int squareSize, int power)
+        {
+            var field = PowerCells.CreatePowerField(input);
+            var (resultX, resultY, resultPower) = PowerCells.CalculatePowerForSquare(input, field, squareSize);
+            resultX.Should().Be(x);
+            resultY.Should().Be(y);
+            resultPower.Should().Be(power);
+        }
+    }
+}
